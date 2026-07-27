885351e52c6e456a3a92307c21b2c87d1ba8484c
jnadeau207-collab <jnadeau207@gmail.com>
2026-07-27 16:53:28 -0400

    Expand render storage for the articulated field engineer

--- native/src/renderer/render_scene.h
+++ native/src/renderer/render_scene.h
@@ -45,7 +45,7 @@ struct RenderScene
     DirectX::XMFLOAT3 player_velocity{};
     float target_exposure = 1.0f;
     bool mission_complete = false;
-    std::array<RenderInstance, 8> supplemental_character_instances{};
+    std::array<RenderInstance, 20> supplemental_character_instances{};
     std::uint32_t supplemental_character_count = 0;
     std::span<const RenderInstance> instances{};
 };
